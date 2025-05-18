a0bfe50f94e94b89cb65f6448d2644bcfb120de5
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-05-18 19:44:41 +0530

    zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import {useCounter} from './store';
 
 const App = () => {
+  const {count,increment,decrement} = useCounter();
+
+
   return <div>
-    eeeeeeeeeeeee
+    <h1>
+      counter: {count}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={() => useCounter.setState({ count: 0 })}>reset</button>
+      
+    </h1>
   </div>;
 };
 
